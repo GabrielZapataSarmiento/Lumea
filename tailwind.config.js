1e699f77aab510a0b7cbf59c97634266691fcb61
Marc Anthony <marc.surmont@student.kdg.be>
2024-05-15 20:09:43 +0200

    Tailwindcss fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/js/**/*.js',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+        './node_modules/tw-elements/js/**/*.js',
+        './resources/views/*.blade.php',
+        "./resources/**/**/**/**/*.blade.php",
     ],
 
     theme: {
