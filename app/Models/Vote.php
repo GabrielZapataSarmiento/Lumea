4c98dd7d353b6283abe48c77b674f89bfdec1a65
Marc Anthony <marc.surmont@student.kdg.be>
2024-05-18 23:51:21 +0200

    Create model Vote

---
+++ app/Models/Vote.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vote extends Model
+{
+    use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function song()
+    {
+        return $this->belongsTo(Song::class);
+    }
+
+    protected $fillable = [
+        'user_id',
+        'song_id',
+        'vote_type'
+    ];
+}
