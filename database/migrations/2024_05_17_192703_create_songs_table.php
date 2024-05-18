b4bf95360aed578d98ef40274e126ff832c1f22f
Marc Anthony <marc.surmont@student.kdg.be>
2024-05-18 23:46:23 +0200

    Create songs table migration

---
+++ database/migrations/2024_05_17_192703_create_songs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false);
+            $table->string('artist')->nullable(false);
+            $table->string('cover_path')->nullable();
+            $table->string('song_path')->nullable();
+            $table->timestamps();
+            $table->softdeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('songs');
+    }
+};
