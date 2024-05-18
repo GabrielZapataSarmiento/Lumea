be41bcbc4902735163483b40b07e2c0ed7d7f40b
Marc Anthony <marc.surmont@student.kdg.be>
2024-05-18 23:49:02 +0200

    Update Appcontroller

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Song;
 use Illuminate\Http\Request;
 
 class AppController extends Controller
 {
+    public function index()
+    {
+        $user = auth()->user();
+        $votedSongIds = $user->votes->pluck('song_id')->toArray();
 
+        $songs = Song::whereNotIn('id', $votedSongIds)->get();
+        return view('app', compact('songs'));
+    }
 }
